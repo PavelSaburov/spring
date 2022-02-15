c633f56bb37e657abd45afd09180d4346d4693ea
psaburov <psaburov@diasoft.ru>
2022-02-15 20:54:21 +0300

    homework rest

--- src/main/java/ru/diasoft/spring/repository/GenreRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.diasoft.spring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.diasoft.spring.entity.Genre;
-
-public interface GenreRepository extends JpaRepository<Genre, Long> {
-
-}
